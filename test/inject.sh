064976afce4ff016e0dccb2add49fd92309103ad
chriso13541 <chriso13541@gmail.com>
2026-04-16 20:11:40 -0400

    Correct terminal

--- test/inject.sh
+++ test/inject.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-DYLIB="$(dirname $0)/tarnish.dylib"
-APP=${1:-/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal}
+DYLIB="$(dirname $0)/../tarnish.dylib"
+APP=${1:-/System/Applications/TextEdit.app/Contents/MacOS/TextEdit}
 
-echo "[Tarnish] Injecting into: $APP"
+echo "[Tarnish] Injecting into: $(basename $APP)"
 DYLD_INSERT_LIBRARIES="$DYLIB" \
 DYLD_FORCE_FLAT_NAMESPACE=1 \
 "$APP"
